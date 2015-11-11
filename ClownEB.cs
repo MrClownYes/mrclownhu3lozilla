636c8914e8ea3354e1e8e96dd9d168aafbeaf477
MrClown <loleo7@hotmail.com>
2015-11-11 17:31:44 -0200

    ClownRiven 5.22
    HU3

---
+++ ClownEB.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+//<!--
+//document.write(unescape('%43%72%69%61%64%6F%20%70%6F%72%20%4D%72%43%6C%6F%77%6E%20%70%61%72%61%20%45%6C%6F%42%75%64%64%79%0A'));
+//-->
+
+//DO NOT DELETE THIS CLASS DO NOT DELETE THIS CLASS DO NOT DELETE THIS CLASS DO NOT DELETE THIS CLASS
---
+++ Extentions.cs
@@ -0,0 +1,18 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+
+namespace ClownRiven
+{
+    internal static class Extentions
+    {
+        public static bool InRange(this Spell.SpellBase s, Obj_AI_Base target)
+        {
+            return ObjectManager.Player.Distance(target) <= s.Range;
+        }
+
+        public static float GetTrueRange(this Spell.SpellBase s, Obj_AI_Base target)
+        {
+            return s.Range;
+        }
+    }
+}
